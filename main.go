f479c77eb9754523ef98d8a3d7e89e723dea1897
Chasmyr <lestratgabin5886@gmail.com>
2024-03-04 12:23:41 +0100

    fix routes package

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/EpitechMscProPromo2025/T-WEB-800-REN_8/router"
+	"github.com/EpitechMscProPromo2025/T-WEB-800-REN_8/routes"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
 
-	router.UseSubNavRoute(e)
+	routes.UseSubNavRoute(e)
 
 	e.Static("/css", "css")
 	e.Static("/static", "static")
