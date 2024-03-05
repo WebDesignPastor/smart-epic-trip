0d3c203b31a3f1129b47d0103b6dff5f144f1414
parmentier <webcreationtp@gmail.com>
2024-03-05 16:45:08 +0100

    1h a tourner en rond

--- routes/nav.go
+++ routes/nav.go
@@ -2,16 +2,31 @@ package routes
 
 import (
 	"context"
+	"log"
+	"os"
 
 	"github.com/EpitechMscProPromo2025/T-WEB-800-REN_8/templates"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func UseSubNavRoute(e *echo.Echo) {
-	// main menu
-	component := templates.Index()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
+	apiKey := os.Getenv("GOOGLE_MAPS_API_KEY")
+	log.Printf("Loaded API key: %s\n", apiKey)
+	if apiKey == "" {
+		log.Println("Warning: GOOGLE_MAPS_API_KEY environment variable is not set.")
+	}
+
+	// main menu
 	e.GET("/", func(c echo.Context) error {
-		return component.Render(context.Background(), c.Response().Writer)
+		ctx := context.WithValue(context.Background(), "APIKey", apiKey)
+		log.Printf("Loaded API key: %s\n", apiKey)
+
+		return templates.Index(apiKey).Render(ctx, c.Response().Writer)
 	})
 }
