e7c68d82fefef3f3dd60a8046fa09b714c153437
dhall3y <dhalley@null.net>
2024-04-21 23:14:22 +0200

    user test

--- back/handler/user_test.go
+++ back/handler/user_test.go
@@ -27,3 +27,35 @@ func TestSignUpCaseSuccess(t *testing.T) {
 		assert.NotEmpty(t, rec.Body.Bytes(), "token")
 	}
 }
+
+func TestLoginCaseSuccess(t *testing.T) {
+	tearDown()
+	setup()
+
+	var (
+		reqJSON = `{"user":{"email":"user4222@test.com","password":"secret"}}`
+	)
+	req := httptest.NewRequest(echo.POST, "/api/users/login", strings.NewReader(reqJSON))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	assert.NoError(t, h.Login(c))
+	if assert.Equal(t, http.StatusOK, rec.Code) {
+		m := responseMap(rec.Body.Bytes(), "user")
+		assert.Equal(t, "user4222", m["username"])
+		assert.Equal(t, "user4222@test.com", m["email"])
+		assert.NotEmpty(t, rec.Body.Bytes(), "token")
+	}
+}
+
+func TestGetAllCaseSuccess(t *testing.T) {
+	tearDown()
+	setup()
+
+	req := httptest.NewRequest(echo.POST, "/api/users/login", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	assert.NoError(t, h.GetAll(c))
+	assert.Equal(t, http.StatusOK, rec.Code)
+}
